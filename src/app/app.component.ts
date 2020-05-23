0639bb9454f73593cf43cdec30b8fee2543cdf39
Wafaa Kameel <wafaa.kameel@el-info.se>
2020-05-23 14:25:39 +0200

    Adding style

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,32 +1,10 @@
-import { Component, OnInit } from '@angular/core';
-import { TodoItem } from './interfaces/todo-item';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent implements OnInit {
-  title = 'todo-list';
-    
-  todoList: TodoItem[] = [
-    { title: 'install NodeJS' },
-    { title: 'install Angular CLI' },
-    { title: 'create new app' },
-    { title: 'serve app' },
-    { title: 'develop app' },
-    { title: 'deploy app' },
-  ];
-  constructor() {
-    
-  }
-
-  ngOnInit() {
-     
-  }
-
-  addItem(value: string) {
-    this.todoList.push({ title: value });
-  }
-
+export class AppComponent {
+  title = 'My To Do List APP';
 }
