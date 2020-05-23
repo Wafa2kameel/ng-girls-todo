dc2cfaf7eb9d8d1578248767b55b46487748cbfa
Wafaa Kameel <wafaa.kameel@el-info.se>
2020-05-23 14:05:30 +0200

    Add items with emit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -25,4 +25,8 @@ export class AppComponent implements OnInit {
      
   }
 
+  addItem(value: string) {
+    this.todoList.push({ title: value });
+  }
+
 }
--- src/app/input-button-unit/input-button-unit.component.ts
+++ src/app/input-button-unit/input-button-unit.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Output, EventEmitter } from '@angular/core';
 
 @Component({
   selector: 'app-input-button-unit',
@@ -7,14 +7,16 @@ import { Component, OnInit } from '@angular/core';
 })
 export class InputButtonUnitComponent implements OnInit {
 
+  @Output() submit: EventEmitter<string> = new EventEmitter();
+
   title = 'Hello World';
 
   constructor() { }
 
   ngOnInit(): void {
-  }
+  } 
 
-  changeTitle(newTitle: string) {
-    this.title = newTitle;
+  submitValue(newTitle: string) {
+    this.submit.emit(newTitle);
   }
 }
